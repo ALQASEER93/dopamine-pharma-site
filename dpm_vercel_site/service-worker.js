caeda7cd162415d1a7f53027fc67af15fa8e76da
ALQASEER93 <92228293+ALQASEER93@users.noreply.github.com>
2025-08-09 03:33:04 +0300

    Delete dpm_vercel_site directory

--- dpm_vercel_site/service-worker.js
+++
@@ -1,8 +0,0 @@
-
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('dpm-v1').then(c=> c.addAll([
-    '/','/index.html','/products.html','/contact.html','/game.html',
-    '/assets/style.css','/assets/i18n.js','/assets/i18n/en.json','/assets/i18n/ar.json','/assets/game.js','/assets/logo.svg'
-  ])));
-});
-self.addEventListener('fetch', (e)=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
